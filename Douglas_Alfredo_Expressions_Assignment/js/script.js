31674f58b3575ca2421b12619ea22ffbc5c92b0c
aadvance <adouglas@fullsail.edu>
2015-02-12 19:49:23 -0800

    Creation & variables
    created new project, included array for family names

---
+++ Douglas_Alfredo_Expressions_Assignment/js/script.js
@@ -0,0 +1,8 @@
+/*
+ Alfredo Douglas
+ February 12, 2015
+ Expressions Assignment
+ */
+//An array listing names of family with vehicles needing gasoline
+var familyNames = ["Mark", "Mary", "James", "Sally"];
+//console.log(familyNames);
