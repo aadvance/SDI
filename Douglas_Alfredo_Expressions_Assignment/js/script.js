dad303246359f47d97ba0b708d7efdf61180bda8
aadvance <adouglas@fullsail.edu>
2015-02-12 20:09:57 -0800

    final language
    added console print with final language

--- Douglas_Alfredo_Expressions_Assignment/js/script.js
+++ Douglas_Alfredo_Expressions_Assignment/js/script.js
@@ -13,6 +13,13 @@ var gallonsMary = prompt("Now let's see about his wife, Mary. She drives a hybri
 var gallonsJames = prompt("Great. Now we're at the son, James. He drives a pickup truck.\n\nHow many for his truck?");
 var gallonsSally = prompt("Last one! Their daughter, Sally, just bought a subcompact car \nthat gets great gas mileage.\n\nHow many gallons for her small tank?");
 //console.log(gallonsMary);
-
+//variable for total number of gallons of gasoline
+var gallonsTotal = Number(gallonsMark) + Number(gallonsMary) + Number(gallonsJames) + Number(gallonsSally);
+alert("Thanks! That makes a total of " + gallonsTotal + " gallons they would need.")
 //Including additional variable prompting user for gas price in their area
-var priceGallon = prompt("Great! Now, how much do you pay per gallon of gas \nin your immediate area?");
+
+//variable for total value when all gallons are added, then multiplied by cost per gallon
+var priceGallon = prompt("Now, how much do you pay per gallon of gas \nin your immediate area?");
+var cost = gallonsTotal * priceGallon;
+//console print with final language
+console.log("Based on the total amount of " + gallonsTotal + " for all vehicles, at a price per gallon of $" + priceGallon + ", the total cost for this family to fill all of their vehicles' tanks with gasoline would be $" + cost + ".")
